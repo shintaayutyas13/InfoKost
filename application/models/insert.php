24ce7821de7bb2cc52e13b86e9dce134878bc687
shintaayutyas13 <47978098+shintaayutyas13@users.noreply.github.com>
2019-04-23 19:58:46 +0700

    Update insert.php

--- application/models/insert.php
+++ application/models/insert.php
@@ -14,4 +14,32 @@ class insert extends CI_Model {
 		$this->db->insert('user',$arrData);
 	}
 }
+
+public function Daftar(){
+		 require_once("config.php");
+    
+    if(isset($_POST['submit'])){
+    	$jk_input = $_POST['jk_input'];
+		$name_input = $_POST['name_input'];
+		$loc_input = $_POST['loc_input'];
+		$fk_input = $_POST['fk_input'];
+		$Luas_input = $_POST['Luas_input'];
+		$mnd_input = $_POST['mnd_input'];
+		$fp_input = $_POST['fp_input'];
+		$al_input = $_POST['al_input'];
+		$dpk_input = $_POST['dpk_input'];
+		$kbs_input = $_POST['kbs_input'];
+		$kl_input = $_POST['kl_input'];
+		$kb_input = $_POST['kb_input'];
+		$dk_input = $_POST['dk_input'];
+
+    	$get = $conn -> query("INSERT INTO `datakost`(jk_input, name_input, loc_input, fk_input, Luas_input, mnd_input, fp_input, al_input , dpk_input, kbs_input, kl_input, kb_input, dk_input) VALUES ('$jk_input','$name_input','$loc_input','$fk_input','$Luas_input','$mnd_input','$fp_input','$al_input' ,'$dpk_input','$kbs_input','$kl_input','$kb_input','$dk_input')");
+    	if($get){
+    		echo "selamat bergabungg";   		
+    	}
+    	else{
+    		echo "maaf kostanmu belum terdaftar";
+    	}
+	}
+}
 ?>
